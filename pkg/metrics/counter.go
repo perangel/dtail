1d2976deb894ce2f359bfe4dc7f11fe536475ea9
Angel Perez <perangel@gmail.com>
2019-02-11 10:49:23 -0500

    fix: use sync/atomic for Counters

--- pkg/metrics/counter.go
+++ pkg/metrics/counter.go
@@ -1,59 +1,62 @@
 package metrics
 
+import "sync/atomic"
+
 // Counter is a simple int64 counter
-type Counter int64
+type Counter struct {
+	i int64
+}
 
 // NewCounter returns a new Counter
 func NewCounter() *Counter {
-	return new(Counter)
+	return &Counter{0}
 }
 
 // NewCounterWithValue initializes and returns a new Counter with a specified value
 func NewCounterWithValue(i int64) *Counter {
-	c := Counter(i)
-	return &c
+	return &Counter{i}
 }
 
 // Value returns the current value of the Counter
 func (c *Counter) Value() int64 {
-	return int64(*c)
+	return c.i
 }
 
 // Inc increments a Counter by a specified amount
 func (c *Counter) Inc(i int64) {
-	*c += Counter(i)
+	atomic.AddInt64(&c.i, 1)
 }
 
 // Add adds the value of another Counter
 func (c *Counter) Add(other Metric) {
 	o := other.(*Counter)
-	*c += *o
+	atomic.AddInt64(&c.i, o.i)
 }
 
 // Multiply multiplies self by another Metric
 func (c *Counter) Multiply(other Metric) {
 	o := other.(*Counter)
-	*c *= *o
+	c.i *= o.i
 }
 
 // Less compares self to another Metric
 func (c *Counter) Less(other Metric) bool {
 	o := other.(*Counter)
-	return *c < *o
+	return c.i < o.i
 }
 
 // Reset resets the Counter to zero
 func (c *Counter) Reset() {
-	*c = 0
+	atomic.SwapInt64(&c.i, 0)
 }
 
 // Clone returns a copy of a Counter
 func (c *Counter) Clone() Metric {
-	nc := Counter(*c)
-	return &nc
+	copy := atomic.LoadInt64(&c.i)
+	return &Counter{copy}
 }
 
 // Float returns the Counter' value as float64
 func (c *Counter) Float() float64 {
-	return float64(*c)
+	return float64(c.i)
 }
